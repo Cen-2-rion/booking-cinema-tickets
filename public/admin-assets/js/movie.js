bdf2b55c8f3f56bbe8372c760c845ce3c7bb0bd8
Cen-2-rion <atrohov1982@gmail.com>
2025-08-27 02:16:38 +0300

    fix

--- public/admin-assets/js/movie.js
+++ public/admin-assets/js/movie.js
@@ -71,7 +71,7 @@ export function movie(csrf, data) {
         movie.addEventListener('dblclick', () => {
             const id = movie.dataset.movieId;
             const movieTitle = movie.querySelector('.conf-step__movie-title').textContent;
-            popupRemove.querySelector('#remove-movies-title').textContent = `\"${movieTitle}\"`;
+            popupRemove.querySelector('#remove-movie-title').textContent = `\"${movieTitle}\"`;
             popupRemove.classList.add('active');
             popupRemove.dataset.movieId = id;
         });
